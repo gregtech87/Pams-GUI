84c9ddab14419e71f8aac0f449c049c5e1e6b15d
Tobbe <gregtobbe@hotmail.com>
2025-04-10 13:23:41 +0200

    Edit to new base url

--- javascript/shared.js
+++ javascript/shared.js
@@ -11,7 +11,7 @@ const weekDaysForPrintout = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 const mainDiv = document.querySelector("#app")
 const messageDiv = document.querySelector("#messageBoxes");
 let windowSize = "width=" + window.innerWidth + ",height=" + window.innerHeight + ",scrollbars=no";
-let baseFetchUrl = 'https://pam-api.gregtech.duckdns.org/api/v1/';
+let baseFetchUrl = 'https://pam-api.gregtech.org/';
 // let baseFetchUrl = 'http://localhost:8586/api/v1/';
 let loggedInUser;
 let base64credentials;
